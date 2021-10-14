38b3f743e56d86433bad0096ca7b5d465a59b1f4
Shubham chaursiya <shubham.chaursiya@hotwaxsystem.com>
2021-10-14 09:42:34 +0530

    Second branch

---
+++ file6.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scan=new Scanner(System.in);
+        int arr[]={2,5,8,4};
+        Arrays.sort(arr);
+System.out.println("Second branch");
+        for(int i=0;i<arr.length;i++) {
+
+            System.out.println(arr[i]);
+        }
+    }
+}
